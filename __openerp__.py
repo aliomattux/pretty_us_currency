4aaa169071ee454bc393bbcd8c900e3eb1a6ca95
root <root@odoo.htm-mbs.com>
2015-09-06 04:48:19 -0400

    First Commit

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Pretty US Currency',
+    'version': '1.1',
+    'author': 'Kyle Waid',
+    'category': 'Sales Management',
+    'depends': ['sale', 'purchase'],
+    'website': 'https://www.gcotech.com',
+    'description': """ 
+    """,
+    'data': ['views/sale.xml',
+	     'views/purchase.xml',
+	     'views/product.xml',
+	     'views/invoice.xml',
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
